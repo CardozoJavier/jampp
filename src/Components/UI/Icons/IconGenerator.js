175fafcc22a977a8eeea5eaa06c02be4e216c9df
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2019-11-18 21:29:14 -0300

    Icons factory function created

---
+++ src/Components/UI/Icons/IconGenerator.js
@@ -0,0 +1,9 @@
+/**
+ * Excecute function for render icon
+ */
+const IconGenerator = ({ renderIcon, props, disabled }) => (
+  renderIcon({ props, disabled })
+);
+
+export default IconGenerator;
+
