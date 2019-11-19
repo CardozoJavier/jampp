d5eabffe0e764b21df313a5b6654b06789bfcb92
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2019-11-18 21:28:27 -0300

    Story for button with icons created

--- stories/Button.stories.js
+++ stories/Button.stories.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import '../src/main.css';
+import { palette } from '../src/Components/styles';
+import {
+  AddIcon,
+  IconGenerator,
+  DownChevronIcon,
+  DownloadFileIcon,
+} from '../src/Components/UI/Icons';
+
 import { 
   PrimaryButtonContainer,
   SecondaryButtonContainer,
+  AccessoryButtonContainer,
   OptionButtonContainer,
 } from '../src/Components/Button/styles';
 
@@ -87,5 +96,60 @@ export const Option = () => (
         <Button label="Choice" className="button--option-right__default" />
       </OptionButtonContainer>
     </OptionGroup>
+  </>
+);
+
+export const Accesory = () => (
+  <>
+    <AccessoryButtonContainer>
+      <IconGenerator
+        renderIcon={AddIcon}
+        props={{
+          left: '20px',
+          fill: palette.white,
+          width: '11px',
+          height: '11px',
+          
+        }}
+      />
+      <Button label="Button" className="button--accessory-left" />
+    </AccessoryButtonContainer>
+    
+    <AccessoryButtonContainer disabled={true}>
+      <IconGenerator
+        renderIcon={AddIcon}
+        props={{
+          left: '20px',
+          fill: palette.gray.g4,
+          width: '11px',
+          height: '11px',
+          
+        }}
+      />
+      <Button label="Button" className="button--accessory-left__inverted" />    
+    </AccessoryButtonContainer>
+
+    <AccessoryButtonContainer>
+      <IconGenerator
+        renderIcon={DownloadFileIcon}
+        props={{
+          left: '28px',
+          fill: palette.gray.g4,
+          width: '20px',
+          height: '20px',
+          
+        }}
+      />
+      <Button label="Export" className="button--accessory-both__inverted" />
+      <IconGenerator
+        renderIcon={DownChevronIcon}
+        props={{
+          right: '20px',
+          fill: palette.gray.g4,
+          width: '16px',
+          height: '16px',
+        }}
+      />
+    </AccessoryButtonContainer>
   </>
 );
