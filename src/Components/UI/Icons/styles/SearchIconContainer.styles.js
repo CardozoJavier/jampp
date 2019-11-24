bed7d56fb882470809102e12cd808547b3206119
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2019-11-24 18:41:13 -0300

    Search icon created

---
+++ src/Components/UI/Icons/styles/SearchIconContainer.styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export default styled.div`
+  position: relative;
+  top: ${({ top }) => top};
+  right: ${({ right }) => right};
+  bottom: ${({ bottom }) => bottom};
+  left: ${({ left }) => left};
+
+  svg {
+    fill: ${({ fill }) => fill};
+    width: ${({ width }) => width};
+    height: ${({ height }) => height};
+    vertical-align: middle;
+  }
+`;
