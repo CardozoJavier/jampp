e4d5999dce2332e77b0392404703d8da18629f65
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2019-11-24 18:48:25 -0300

    Stories container fixed

--- stories/styles/StoriesContainer.styles.js
+++ stories/styles/StoriesContainer.styles.js
@@ -3,8 +3,8 @@ import styled from 'styled-components';
 export const StoriesContainer = styled.div`
   display: flex;
   justify-content: space-evenly;
-  align-items: center;
   margin: 10% 0;
+
 `;
 
 export const OptionGroup = styled.div`
