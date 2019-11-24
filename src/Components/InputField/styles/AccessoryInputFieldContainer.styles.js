de0148b907404637b85aca5e60106107b4bf917d
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2019-11-24 18:56:20 -0300

    Container for inputField with accessories created

---
+++ src/Components/InputField/styles/AccessoryInputFieldContainer.styles.js
@@ -0,0 +1,73 @@
+import styled from 'styled-components';
+import { palette } from '../../styles';
+const { gray } = palette;
+
+/**
+ * Accessory input field - left | right | both
+ */
+export default styled.div`
+  .input {
+    transition: all .3s;
+    border: 1px solid ${gray.g1};
+    color: ${gray.g4};
+
+    &::placeholder {
+      color: ${gray.g07};
+    }
+
+    &-large {}
+
+    &-medium {
+      padding: .8em;
+    }
+
+    &-small {}
+
+    &.accessory {
+      border-radius: 4px;
+      position: absolute;
+
+      &--default {
+        &__left {
+          padding: .8em .7em .8em 3em;
+        }
+  
+        &__right {
+          padding: .8em 3em .8em .7em;
+        }
+  
+        &__both {
+          padding: .8em 3em;
+        }
+
+        &:hover, &:focus {
+          transition: all .3s;
+          border: 1px solid ${gray.g3};
+      
+          &::placeholder {
+            color: ${gray.g3};
+          }
+        }
+
+        &__disabled {
+          cursor: default;
+          background-color: ${gray.g0};
+          border: 1px solid ${gray.g1}
+          &::placeholder {
+            color: ${gray.g05};
+          }
+
+          &:hover, &:focus {
+            cursor: default;
+            background-color: ${gray.g0};
+            border: 1px solid ${gray.g1}
+            &::placeholder {
+              color: ${gray.g07};
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+  
