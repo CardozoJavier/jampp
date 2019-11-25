f090be82ef5a92c55ca1097ac41653d5ec120ea1
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2019-11-24 21:09:10 -0300

    Modularizing to function utility

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -1,20 +1,10 @@
 import React, { useState } from 'react';
 import { ButtonInput } from './styles';
-import { bemDestruct } from '../../utils';
-
-const getClassName = (className, defaultClassName, optionalClassName) => {
-  let toggleToClassName;
-  if (optionalClassName) {
-    toggleToClassName = className === defaultClassName ? optionalClassName : defaultClassName;
-    return toggleToClassName;
-  } else {
-    return className;
-  }
-};
+import { bemDestruct, getClassName } from '../../utils';
 
 const triggerAction = () => setTimeout(() => alert('You clicked me!'), 1000);
 
-export default ({ label, children, defaultClassName, optionalClassName, disabled }) => {
+const Button = ({ label, children, defaultClassName, optionalClassName, disabled }) => {
   const [className, setClassName] = useState(defaultClassName);
   const toggleToClassName = getClassName(className, defaultClassName, optionalClassName);
 
@@ -31,3 +21,5 @@ export default ({ label, children, defaultClassName, optionalClassName, disabled
     />
   );
 };
+
+export default Button;
