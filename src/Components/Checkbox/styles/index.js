f148069079d0de8695bad6671d3c48ed7309944d
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2019-11-26 22:13:55 -0300

    Classes created for default/selected checkbox

---
+++ src/Components/Checkbox/styles/index.js
@@ -0,0 +1,4 @@
+export { Box } from './Box.styles';
+export { OptionGroup } from './OptionGroup.styles';
+export { CheckboxLabel } from './CheckboxLabel.styles';
+export { CheckboxContainer } from './CheckboxContainer.styles';
