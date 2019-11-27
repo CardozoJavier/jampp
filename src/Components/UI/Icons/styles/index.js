59504c039ec7e0d209c81b2d85409fd44834f844
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2019-11-26 22:11:35 -0300

    Icon check created

--- src/Components/UI/Icons/styles/index.js
+++ src/Components/UI/Icons/styles/index.js
@@ -5,3 +5,4 @@ export { default as EllipseIconContainer } from './EllipseIconContainer.styles';
 export { default as XIconContainer } from './XIconContainer.styles';
 export { default as InfoIconContainer } from './InfoIconContainer.styles';
 export { default as SearchIconContainer } from './SearchIconContainer.styles';
+export { default as CheckIconContainer } from './CheckIconContainer.styles';
