b5459f7f20c807ad9604a186e3e919db3e0d717e
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2019-11-29 19:38:46 -0300

    Belt icon and container created

---
+++ src/Components/UI/Icons/styles/BeltIconContainer.styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export default styled.div`
+  top: ${({ top }) => top};
+  right: ${({ right }) => right};
+  bottom: ${({ bottom }) => bottom};
+  left: ${({ left }) => left};
+  margin: ${({ margin }) => margin};
+
+  svg {
+    fill: ${({ fill }) => fill};
+    width: ${({ width }) => width};
+    height: ${({ height }) => height};
+    vertical-align: middle;
+  }
+`;
