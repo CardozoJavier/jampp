9ab6d8b8e7bfba23cb3e831fa4ddd4f1249a8d25
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2019-12-10 22:18:18 -0300

    Refactor and convert to presentational component

--- src/Components/MultipleOptionList/MultipleOptionList.js
+++ src/Components/MultipleOptionList/MultipleOptionList.js
@@ -1,33 +1,15 @@
 import React from 'react';
-import { MultipleOptionListContainer } from './styles';
-import { CheckIcon } from '../UI/Icons';
-import { palette } from '../styles';
-import { Checkbox } from '../Checkbox';
-import { bemDestruct } from '../../utils';
+import { MultipleOptionListContainer, OptionContainer } from './styles';
+import { bemDestruct, isLastItem } from '../../utils';
 
-const { action, white, } = palette;
-
-const MultipleOptionList = ({ className, options, }) => {
-  return (
-    <MultipleOptionListContainer className={bemDestruct(className)}>
-      {options.map(option => (
-        <Checkbox
-          key={option.label}
-          label={option.label}
-          Icon={CheckIcon}
-          iconProps={{
-            right: '13px',
-            top: '5px',
-            fill: white,
-            width: '9px',
-            height: '7px',
-          }}
-          defaultClassName={option.defaultClassName}
-          optionalClassName={option.optionalClassName}
-        />
-      ))}  
-    </MultipleOptionListContainer>
-  );
-};
+const MultipleOptionList = ({ options, className }) => (
+  <MultipleOptionListContainer className={bemDestruct(className)}>
+    {options.map((option, index) => (
+      <OptionContainer className={`last-item__${isLastItem(options.length, index)}`}>
+        {option}
+      </OptionContainer>
+    ))}
+  </MultipleOptionListContainer>
+);
 
 export default MultipleOptionList;
