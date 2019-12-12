711d9debd88e2c05a4ec8db436063d42471b5baa
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2019-12-12 01:27:19 -0300

    Styles modified for checkbox in dropdowns

---
+++ src/Components/OptionList/styles/OptionCheckboxGroup.styles.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export default styled.div`
+  position: absolute;
+  right: 0;
+  top: 45px;
+  border-radius: 4px;
+  box-shadow: 0 4px 16px rgba(0, 0, 0, .1);
+
+  &.dropdown {
+    &-right {}
+    
+    &.option, &.label, &.button {
+      &--default, &--gray {
+        &__opened {
+          opacity: 1;
+          visibility: visible;
+          transform: translate(0, 10px);
+          transition: all .3s;
+        }
+
+        &__closed {
+          opacity: 0;
+          visibility: hidden;
+          transform: translate(0, 0);
+          transition: all .3s;
+        }
+      }
+    }
+  }
+`;
---
+++ src/Components/UniqueOption/styles/Container.styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+import { Container } from '../../Checkbox/styles';
+
+export const WrapperContainer = styled(Container)`
+
+`;
