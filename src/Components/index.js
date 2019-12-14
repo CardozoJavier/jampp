84ee796cead4f228c77f22568f3be9561252ef56
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2019-12-14 19:00:25 -0300

    Containers for text created

--- src/Components/index.js
+++ src/Components/index.js
@@ -11,3 +11,4 @@ export { OptionList } from './OptionList';
 export { StatusLabel } from './StatusLabel';
 export { UniqueOption } from './UniqueOption';
 export { ControlledInputField } from './InputField';
+export { NotificationHeader } from './NotificationHeader';
