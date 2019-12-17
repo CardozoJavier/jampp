ac559a9324e959c4c6bc50fc73234b709129be2b
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2019-12-16 23:36:44 -0300

    Refactor in label stories

--- stories/Label.stories.js
+++ stories/Label.stories.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import '../src/main.css';
-import { palette } from '../src/Components/styles';
 import {
-  IconGenerator,
   EllipseIcon,
   XIcon,
 } from '../src/Components/UI/Icons';
-
-import {
-  StatusButtonContainer,
-} from '../src/Components/Button/styles';
 
 import { StatusLabelContainer } from './styles/StoriesContainer.styles';
 
@@ -20,87 +14,28 @@ export default {
   title: 'Label',
 };
 
-const { white, black, action, link, gray, violet, green, blue, yellow, red } = palette;
 
 export const Default = () => (
   <>
-      <Button label="Label" type="label-small-right" icon={XIcon} />
-      <Button label="Label" type="label-default-medium" icon={XIcon} />
+    <Button label="Label" type="label-default-small" icon={XIcon} />
+    <Button label="Label" type="label-default-medium" icon={XIcon} />
   </>
 );
 
 export const Status = () => (
   <StatusLabelContainer>
     <LabelContainer>
-      <StatusButtonContainer>
-        <Button label="Status" defaultClassName="button status--green-small" />
-      </StatusButtonContainer>
-
-      <StatusButtonContainer>
-        <Button label="Status" defaultClassName="button status--red-small" />
-      </StatusButtonContainer>
-
-      <StatusButtonContainer>
-        <Button label="Status" defaultClassName="button status--yellow-small" />
-      </StatusButtonContainer>
-
-      <StatusButtonContainer>
-        <Button label="Status" defaultClassName="button status--blue-small" />
-      </StatusButtonContainer>
+      <Button label="Status" type="status-small-green" />
+      <Button label="Status" type="status-small-red" />
+      <Button label="Status" type="status-small-yellow" />
+      <Button label="Status" type="status-small-blue" />
     </LabelContainer>
 
     <LabelContainer>
-      <StatusButtonContainer>
-        <IconGenerator 
-          renderIcon={EllipseIcon} 
-          props={{
-            left: '8px',
-            fill: green.g1,
-            width: '6px',
-            height: '6px',
-          }}
-          />
-        <Button label="Status" defaultClassName="button status--green-small__left" />
-      </StatusButtonContainer>
-
-      <StatusButtonContainer>
-        <IconGenerator 
-          renderIcon={EllipseIcon} 
-          props={{
-            left: '8px',
-            fill: red.r3,
-            width: '6px',
-            height: '6px',
-          }}
-          />
-        <Button label="Status" defaultClassName="button status--red-small__left" />
-      </StatusButtonContainer>
-
-      <StatusButtonContainer>
-        <IconGenerator 
-          renderIcon={EllipseIcon} 
-          props={{
-            left: '8px',
-            fill: yellow.y2,
-            width: '6px',
-            height: '6px',
-          }}
-          />
-        <Button label="Status" defaultClassName="button status--yellow-small__left" />
-      </StatusButtonContainer>
-
-      <StatusButtonContainer>
-        <IconGenerator 
-          renderIcon={EllipseIcon} 
-          props={{
-            left: '8px',
-            fill: link,
-            width: '6px',
-            height: '6px',
-          }}
-          />
-        <Button label="Status" defaultClassName="button status--blue-small__left" />
-      </StatusButtonContainer>
+      <Button label="Status" type="status-small-green" icon={EllipseIcon} />
+      <Button label="Status" type="status-small-red" icon={EllipseIcon} />
+      <Button label="Status" type="status-small-yellow" icon={EllipseIcon} />
+      <Button label="Status" type="status-small-blue" icon={EllipseIcon} />
     </LabelContainer>
   </StatusLabelContainer>
 );
