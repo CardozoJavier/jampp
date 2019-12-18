07341440b67b740b8649f1c74b3ee6c62ff8efc4
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2019-12-17 23:32:28 -0300

    Option and label container created

---
+++ src/Components/OptionList/styles/OptionContainer.styles.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+import OptionCheckboxContainer from './OptionCheckboxContainer.styles';
+import { palette } from '../../styles';
+import { CheckIconContainer } from '../../UI/Icons/styles';
+const { action, gray, violet } = palette;
+
+export default styled(OptionCheckboxContainer)`
+  padding: 10px;
+
+  &.unique {
+    &.option {
+      &--default {
+        &__selected {
+          background-color: ${violet.v05};
+          transition: background-color .3s;
+          ${CheckIconContainer} {
+            path {
+              stroke: ${action};
+            }
+          }
+          p {
+            font-weight: 900;
+            color: ${action};
+            transition: font-weight .3s;
+          }
+        }
+      }
+    }
+  }
+`;
