e7f4f8eba2435c7bdc747773c6ab9aad23f2ec7f
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2019-12-17 23:34:28 -0300

    Refactor in SwitchGroup for support SwitchButton

--- src/Components/Switch/SwitchGroup.js
+++ src/Components/Switch/SwitchGroup.js
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 import buttonProps from './buttonProps';
-import { SwitchGroupContainer } from './styles';
+import { SwitchGroupContainer, SwitchButtonInput } from './styles';
 import { bemDestruct, settingClassName } from '../../utils';
 
-const SwitchGroup = ({ children, type, className, OptionItem, disabled }) => {
+const SwitchGroup = ({ children, type, disabled }) => {
   const { defaultClassName, optionalClassName, ButtonContainer, iconProps, iconClassName } = buttonProps[type];
-  const childrenParsed = settingClassName(children);
+  const childrenParsed = settingClassName(children, -1, defaultClassName);
   const [array, setArray] = useState(childrenParsed);
-
+  
   /**
    * When an option is clicked, his className is toggle to selected and everyone else are being uncheck.
    */
@@ -19,56 +19,17 @@ const SwitchGroup = ({ children, type, className, OptionItem, disabled }) => {
   return (
     <SwitchGroupContainer className={bemDestruct(defaultClassName)}>
       {array.map((input) => (
-        <ButtonContainer className={bemDestruct(defaultClassName, disabled)} disabled={disabled}>
-          <OptionItem
-            className={input.className}
-            handleCheck={handleCheck}
-            label={input.label}
-            color={input.color}
-            disabled={disabled}
-            key={input.value}
-            id={input.id}
-          />
-        </ButtonContainer>
-      ))}
+          <ButtonContainer className={bemDestruct(input.className, disabled)} disabled={disabled} key={input.id}>
+            <SwitchButtonInput
+              onClick={disabled ? null : (e) => handleCheck(e, input.id, defaultClassName)}
+              children={input.label || children}
+              color={input.color}
+            />
+          </ButtonContainer>
+        )
+      )}
     </SwitchGroupContainer>
   );
 };
 
 export default SwitchGroup;
-
-
-// import React, { useState } from 'react';
-// import { SwitchGroupContainer } from './styles';
-// import { bemDestruct, settingClassName } from '../../utils';
-
-// const SwitchGroup = ({ children, className, OptionItem, disabled }) => {
-//   const childrenParsed = settingClassName(children);
-//   const [array, setArray] = useState(childrenParsed);
-
-//   /**
-//    * When an option is clicked, his className is toggle to selected and everyone else are being uncheck.
-//    */
-//   const handleCheck = (e, currentValue) => {
-//     const inputsArray = settingClassName(children, currentValue);
-//     setArray(inputsArray);
-//   }
-
-//   return (
-//     <SwitchGroupContainer className={bemDestruct(className)}>
-//       {array.map((input) => (
-//         <OptionItem
-//           key={input.value}
-//           label={input.label}
-//           value={input.value}
-//           color={input.color}
-//           className={input.className}
-//           handleCheck={handleCheck}
-//           disabled={disabled}
-//         />
-//       ))}
-//     </SwitchGroupContainer>
-//   );
-// };
-
-// export default SwitchGroup;
