f07d6665e40c839e77ad2f28991be745adfeea65
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2019-12-17 23:31:10 -0300

    Refactor in optionList component

--- src/Components/OptionList/OptionList.js
+++ src/Components/OptionList/OptionList.js
@@ -1,30 +1,46 @@
 import React, { useState } from 'react';
-import { OptionCheckboxGroup } from './styles';
+import { OptionCheckboxGroup, OptionLabel } from './styles';
 import { bemDestruct, settingClassName } from '../../utils';
+import { CheckIcon, IconGenerator } from '../UI/Icons';
+import { palette } from '../styles';
+import optionListProps from './optionListProps';
 
-const OptionList = ({ children, className, OptionItem }) => {
-  const childrenParsed = settingClassName(children);
+const { white } = palette;
+
+const OptionList = ({ children, type, disabled, className, OptionItem }) => {
+  const { defaultClassName, optionalClassName, OptionContainer, iconProps, iconClassName } = optionListProps[type];
+  const childrenParsed = settingClassName(children, -1, defaultClassName);
   const [array, setArray] = useState(childrenParsed);
 
   /**
    * When an option is clicked, his className is toggle to selected and everyone else are being uncheck.
    */
-  const handleCheck = (e, currentValue) => {
-    const inputsArray = settingClassName(children, currentValue);
+  const handleCheck = (e, id) => {
+    const inputsArray = settingClassName(children, id, defaultClassName);
     setArray(inputsArray);
   }
 
   return (
     <OptionCheckboxGroup className={bemDestruct(className)}>
       {array.map((input) => (
-        <OptionItem
-          key={input.value}
-          label={input.label}
-          value={input.value}
-          color={input.color}
-          className={input.className}
-          handleCheck={handleCheck}
-        />
+        <OptionContainer
+          onClick={(e) => handleCheck(e, input.id, defaultClassName)}
+          className={bemDestruct(input.className, disabled)}
+          disabled={disabled}
+          key={input.id}
+        >
+          <OptionLabel>{ input.label }</OptionLabel>
+          <IconGenerator
+            renderIcon={CheckIcon}
+            props={{
+              right: '10px',
+              fill: white,
+              width: '14px',
+              height: '10px',
+              className: bemDestruct(input.className),
+            }}
+          />
+        </OptionContainer>
       ))}
     </OptionCheckboxGroup>
   );
