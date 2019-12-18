42ca134d22a824a48bdeb048942767b185550e38
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2019-12-17 23:31:42 -0300

    OptionList props decoupled

---
+++ src/Components/OptionList/optionListProps.js
@@ -0,0 +1,23 @@
+import { OptionContainer } from "./styles";
+
+/**
+ * Properties for option list by type
+ */
+const optionListProps = {
+  "unique-option": {
+    defaultClassName: "unique option--default",
+    optionalClassName: "",
+    OptionContainer: OptionContainer,
+    iconClassName: '',
+    iconProps: {},
+  },
+  "status-option": {
+    defaultClassName: "status option--default",
+    optionalClassName: "",
+    OptionContainer: OptionContainer,
+    iconClassName: '',
+    iconProps: {},
+  },
+};
+
+export default optionListProps;
