99266e8a3db6b70458d498d2ac3b7c3d637a4a7e
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2019-12-21 17:35:46 -0300

    InputProps created for set inputs properties

---
+++ src/Components/InputField/inputProps.js
@@ -0,0 +1,54 @@
+import { palette } from '../styles';
+const { gray } = palette;
+
+/**
+ * Properties for inputs field by type
+ */
+const inputProps = {
+  basic: {
+    defaultClassName: 'input basic--default-medium',
+  },
+  "accessory-left": {
+    defaultClassName: 'input accessory--default-medium__left',
+    iconProps: {
+      top: '7px',
+      left: '10px',
+      fill: gray.g07,
+      width: '11px',
+      height: '11px',
+      verticalAlign: 'middle',
+    },
+  },
+  "accessory-right": {
+    defaultClassName: 'input accessory--default-medium__right',
+    iconProps: {
+      top: '7px',
+      left: '145px',
+      fill: palette.gray.g07,
+      width: '16px',
+      height: '16px',
+      verticalAlign: 'middle',
+    },
+  },
+  "label-right": {
+    defaultClassName: 'input accessory--default-medium__right',
+    iconProps: {
+      top: '34px',
+      left: '145px',
+      fill: palette.gray.g07,
+      width: '16px',
+      height: '16px',
+      verticalAlign: 'middle',
+    },
+  },
+  controlled: {
+    inputClassName: "input basic--default-medium",
+    defaultClassName: "placeholder controlled--default-medium",
+    optionalClassName: "placeholder controlled--default-medium__selected",
+  },
+  "searchbar": {
+    defaultClassName: 'input accessory--searchbar-medium__left',
+  },
+};
+
+export default inputProps;
