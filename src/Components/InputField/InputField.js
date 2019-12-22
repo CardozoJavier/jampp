6a872778832055d1911c2d05b199dfb21e8755ed
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2019-12-22 17:33:08 -0300

    InputField component renamed

--- src/Components/InputField/InputField.js
+++ src/Components/InputField/InputField.js
@@ -1,16 +1,74 @@
-import React from 'react';
-import { Input } from './styles';
-import { bemDestruct } from '../../utils';
+import React, { useState } from 'react';
+import { LabelContainer, Input, Label, ErrorMessage } from './styles';
+import { IconGenerator, ExclamationIcon } from '../UI/Icons';
+import { getClassName, bemDestruct } from '../../utils';
+import inputProps from './inputProps';
 
+const InputField = ({ placeholder, errorMessage, label, id, type, icon, disabled }) => {
+  const { defaultClassName, optionalClassName, errorClassName, onBlurClassName, onFocusClassName, InputContainer, iconProps } = inputProps[type];
+  const [className, setClassName] = useState(defaultClassName);
+  const [error, setError] = useState(false);
+  const toggleToClassName = getClassName(className, defaultClassName, optionalClassName);
 
-const InputField = ({ placeholder, className, disabled, id }) => (
-  <Input
-    id={id}
-    placeholder={placeholder}
-    className={bemDestruct(className, disabled)}
-    type="text"
-    disabled={disabled}
-  />
-);
+  const handleClick = () => {
+    setClassName(toggleToClassName);
+  }
+
+  const handleBlur = () => {
+    if (!error) {
+      setClassName(onBlurClassName);
+    }
+  }
+
+  const handleFocus = () => {
+    if (!error) {
+      setClassName(onFocusClassName);
+    }
+  }
+
+  const handleChange = ({ target: { value }}) => {
+    const isDigit = /\d/.test(value);
+
+    if (isDigit) {
+      setError(true);
+      setClassName(errorClassName);
+    } else {
+      setError(false);
+      setClassName(onFocusClassName);
+    }
+  }
+
+  return (
+    <LabelContainer>
+      <Label htmlFor={id}>{label}</Label>
+      <InputContainer
+        onClick={disabled ? null : handleClick}
+        className={bemDestruct(className, disabled)}
+        disabled={disabled}
+      >
+        <Input
+          id={id}
+          placeholder={placeholder}
+          onBlur={disabled ? null : handleBlur}
+          onFocus={disabled ? null : handleFocus}
+          onChange={disabled ? null : handleChange}
+          disabled={disabled}
+        />
+
+        {icon ? <IconGenerator renderIcon={icon} props={iconProps} /> : null}
+
+        {errorMessage !== undefined && 
+          <IconGenerator
+            renderIcon={ExclamationIcon}
+            props={{}}
+          />
+        }
+      </InputContainer>
+      {errorMessage !== undefined && 
+        <ErrorMessage className={bemDestruct(className)}>{errorMessage}</ErrorMessage>
+      }
+    </LabelContainer>
+  );
+};
 
 export default InputField;
