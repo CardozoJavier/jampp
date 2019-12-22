4d8cf028037f86a5dc2d457a0a654dc08095a05b
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2019-12-22 17:36:40 -0300

    Fixed styles in input styled component

--- src/Components/InputField/styles/Input.styles.js
+++ src/Components/InputField/styles/Input.styles.js
@@ -1,6 +1,12 @@
 import styled from 'styled-components';
 
 export default styled.input`
+  width: 100%;
+  border: 0;
+  border-radius: 4px;
+  padding: 10px;
+  font-size: 12px;
+
   &:focus {
     outline: none;
   }
