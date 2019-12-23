0936ae664971172f8167c48c57b0c4f7e7889c6f
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2019-12-23 13:24:20 -0300

    Stories created for radio button

--- src/Components/index.js
+++ src/Components/index.js
@@ -12,6 +12,7 @@ export { InputText } from './InputField';
 export { InputField } from './InputField';
 export { OptionList } from './OptionList';
 export { StatusLabel } from './StatusLabel';
+export { RadioButton } from './RadioButton';
 export { ToggleSwitch } from './ToggleSwitch';
 export { UniqueOption } from './UniqueOption';
 export { NotificationHeader } from './NotificationHeader';
