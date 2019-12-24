9d18724b0d06c3fd5b352f2d5339ec435c9e0a93
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2019-12-24 19:03:12 -0300

    OptionListAccount component created

---
+++ src/Components/OptionList/OptionListAccount.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import {
+  EmailFieldContainer,
+  OptionListContainer,
+  OptionItemContainer,
+  ButtonContainer,
+  EmailField,
+} from './styles';
+import { bemDestruct, settingClassName } from '../../utils';
+import { CheckIcon, IconGenerator, SearchIcon } from '../UI/Icons';
+import { palette } from '../styles';
+import optionListProps from './optionListProps';
+import { InputField } from '../InputField';
+import { Button } from '../Button';
+
+const { white } = palette;
+
+const OptionList = ({ children, type, email, disabled, className, OptionItem }) => {
+  const {
+    iconProps,
+    iconClassName,
+    OptionContainer,
+    defaultClassName,
+    optionalClassName,
+  } = optionListProps[type];
+  const childrenParsed = settingClassName(children, -1, defaultClassName);
+  const [array, setArray] = useState(childrenParsed);
+
+  /**
+   * When an option is clicked, his className is toggle to selected and everyone else are being uncheck.
+   */
+  const handleCheck = (e, id) => {
+    const inputsArray = settingClassName(children, id, defaultClassName);
+    setArray(inputsArray);
+  }
+
+  return (
+    <OptionListContainer className={bemDestruct(className, disabled)}>
+      <InputField placeholder="Filter organization" type="icon-small-left" icon={SearchIcon} />
+      <OptionItemContainer>
+        {array.map((input) => (
+          <OptionItem
+            className={input.className}
+            handleCheck={handleCheck}
+            label={input.label}
+            color={input.color}
+            key={input.id}
+            id={input.id}
+          />
+        ))}
+      </OptionItemContainer>
+      <EmailFieldContainer>
+        <EmailField>{email}</EmailField>
+      </EmailFieldContainer>
+      <ButtonContainer>
+        <Button label="Sign out" type="secondary-gray-medium" fixed />
+      </ButtonContainer>
+    </OptionListContainer>
+  );
+};
+
+export default OptionList;
