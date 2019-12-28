d78891b023da35c705acc16ce5f3be4b05e3e4eb
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2019-12-28 13:33:06 -0300

    Styles for header container created

--- src/Components/Header/styles/Header.styles.js
+++ src/Components/Header/styles/Header.styles.js
@@ -1,4 +1,12 @@
 import styled from 'styled-components';
 
-export const HeaderContainer = styled.div``;
+export const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  background-color: white;
+  align-items: center;
+  padding: 0 10px;
+  height: 52px;
+`;
 
