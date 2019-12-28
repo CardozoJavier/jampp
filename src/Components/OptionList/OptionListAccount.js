bfb596a17e54406485df6dfe75d7c6ed9ca3edaa
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2019-12-28 13:30:23 -0300

    Add check for empty state in dropdown

--- src/Components/OptionList/OptionListAccount.js
+++ src/Components/OptionList/OptionListAccount.js
@@ -23,7 +23,8 @@ const OptionList = ({ children, type, email, disabled, className, OptionItem }) 
     defaultClassName,
     optionalClassName,
   } = optionListProps[type];
-  const childrenParsed = settingClassName(children, -1, defaultClassName);
+  const isThereChildren = !!children;
+  const childrenParsed = isThereChildren ? settingClassName(children, -1, defaultClassName) : [];
   const [array, setArray] = useState(childrenParsed);
 
   /**
@@ -36,22 +37,26 @@ const OptionList = ({ children, type, email, disabled, className, OptionItem }) 
 
   return (
     <OptionListContainer className={bemDestruct(className, disabled)}>
-      <InputField placeholder="Filter organization" type="icon-small-left" icon={SearchIcon} />
-      <OptionItemContainer>
-        {array.map((input) => (
-          <OptionItem
-            className={input.className}
-            handleCheck={handleCheck}
-            label={input.label}
-            color={input.color}
-            key={input.id}
-            id={input.id}
-          />
-        ))}
-      </OptionItemContainer>
-      <EmailFieldContainer>
-        <EmailField>{email}</EmailField>
-      </EmailFieldContainer>
+      {isThereChildren &&
+        <>
+          <InputField placeholder="Filter organization" type="icon-small-left" icon={SearchIcon} />
+          <OptionItemContainer>
+            {array.map((input) => (
+              <OptionItem
+                className={input.className}
+                handleCheck={handleCheck}
+                label={input.label}
+                color={input.color}
+                key={input.id}
+                id={input.id}
+              />
+            ))}
+          </OptionItemContainer>
+          <EmailFieldContainer>
+            <EmailField>{email}</EmailField>
+          </EmailFieldContainer>
+        </>
+      }
       <ButtonContainer>
         <Button label="Sign out" type="secondary-gray-medium" fixed />
       </ButtonContainer>
