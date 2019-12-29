68ce5f20ba508078c3087489ad07ca0079eb73f4
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2019-12-29 18:56:42 -0300

    Refactor in RadioButton component and doc comments added

--- src/Components/RadioButton/RadioButton.js
+++ src/Components/RadioButton/RadioButton.js
@@ -10,16 +10,25 @@ import {
 } from './styles';
 import { bemDestruct } from '../../utils';
 
-const RadioButton = ({ id, name, text = '', value, onChange, checked, suffix, disabled }) => (
+/**
+ * RadioButton component should be called with
+ * @param {id} id - (Required) It's an unique ID to identifier each radiobutton in group.
+ * @param {boolean} checked - (Required) Indicate if tab is selected or not.
+ * @param {string} name - (Required) Name of radiobuttons group necesary to handle check states.
+ * @param {string} text - (Optional) Text to be displayed next to radio button.
+ * @param {function} onChange - (Optional) Callback to trigger on onChange event. It receive the radio button id in first argument.
+ * @param {string} suffix - (Optional) Text to be displayed below of radio button.
+ * @param {boolean} disabled - (Optional) If true, disable actions triggering and styles in component.
+ */
+const RadioButton = ({ id, name, text = '', onChange, checked, suffix, disabled }) => (
   <RadioButtonContainer>
     <InputTypeRadioContainer disabled={disabled}>
       <InputTypeRadio
         id={id}
         type="radio"
         name={name}
-        value={value}
         checked={checked}
-        onChange={e => onChange(id, value, e)}
+        onChange={() => onChange(id)}
       />
       <Radio className={bemDestruct('input radio--visible', disabled)} />
     </InputTypeRadioContainer>
--- src/Components/RadioButton/RadioButtonGroup.js
+++ src/Components/RadioButton/RadioButtonGroup.js
@@ -2,12 +2,19 @@ import React, { useState } from 'react';
 import { RadioButtonGroupContainer } from './styles';
 import RadioButton from './RadioButton';
 
+/**
+ * RadioButtonGroup component should be called with
+ * @param {array} children - (Required) Array of radio buttons to be displayed.
+ * @param {string} name - (Required) String to handle radiobuttons into a group.
+ * @param {string} defaultValue - (Optional)  It's to check a default radio button. It should match with RadioButton id.
+ * @param {function} onChange - (Optional) Callback to trigger on onChange event. It receive the radio button id in first argument.
+ */
 const RadioButtonGroup = ({ children, name, defaultValue, onChange }) => {
   const [selectedOption, setSelectedOption] = useState(defaultValue);
 
-  const handleChange = (id, value, e) => {
+  const handleChange = (id) => {
     setSelectedOption(id);
-    onChange && onChange(value, e);
+    onChange && onChange(id);
   };
 
   return (
@@ -17,10 +24,9 @@ const RadioButtonGroup = ({ children, name, defaultValue, onChange }) => {
           name={name}
           id={radio.props.id}
           key={radio.props.id}
-          onChange={radio.props.disabled ? null : handleChange}
+          onChange={radio.props.disabled ? () => null : handleChange}
           text={radio.props.text}
           suffix={radio.props.suffix}
-          value={radio.props.value}
           disabled={radio.props.disabled}
           checked={selectedOption === radio.props.id}
         />
