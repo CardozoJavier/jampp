f664f4dae2209c5d0b10d0ee3d9a711dd5ff719f
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2019-12-29 12:30:07 -0300

    Doc comments added in toggle stories

--- stories/Toggle.stories.js
+++ stories/Toggle.stories.js
@@ -5,9 +5,16 @@ export default {
   title: 'Toggle',
 };
 
+/**
+ * Toggle component can be called with
+ * @param {string} color - Used for setting the active color on switch. Can be 'green' (default), 'blue', 'red', 'yellow'.
+ * @param {function} onChange - Callback to trigger on onChange event. It receive one argument (true or false).
+ * @param {boolean} disabled - Flag to disable component if param is true.
+ */ 
 export const Toggle = () => (
   <>
-    <ToggleSwitch color="green" />
-    <ToggleSwitch color="green" disabled={true}/>
+    <ToggleSwitch />
+    <ToggleSwitch color="blue" onChange={(status) => setTimeout(() => alert('Toggle status is ' + status), 500)} />
+    <ToggleSwitch color="green" disabled={true} />
   </>
 );
