35deae048da17233971afdb74b798536a89dee28
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2019-12-29 16:56:03 -0300

    Doc comments added in Button component

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -4,6 +4,16 @@ import buttonProps from './buttonProps';
 import { IconGenerator } from '../UI/Icons';
 import { bemDestruct, getClassName } from '../../utils';
 
+/**
+ * Button component should be called with
+ * @param {string} label - (Required) Text to be display inside button.
+ * @param {string} type - (Required) Define button classes for styling.
+ * @param {boolean} fixed - (Optional) Disabled move up transform on hover.
+ * @param {boolean} disabled - (Optional) Disable component if is true.
+ * @param {function} icon - (Optional) Function that returns an svg icon.
+ * @param {function} onClick - (Optional) Callback to trigger on onClick event.
+ * @param {object} children - (Optional) HTML element to be rendered, e.g.: strong tag inside button.
+ */
 const Button = ({ label, children, icon, type, disabled, onClick, fixed }) => {
   const { defaultClassName, optionalClassName, ButtonContainer, iconProps, iconClassName } = buttonProps[type];
 
--- stories/PrimaryButton.stories.js
+++ stories/PrimaryButton.stories.js
@@ -6,6 +6,16 @@ export default {
   title: 'Primary Button',
 };
 
+/**
+ * Button component should be called with
+ * @prop {string} label - (Required) Text to be display inside button.
+ * @prop {string} type - (Required) Define button classes for styling.
+ * @prop {boolean} fixed - (Optional) Disabled move up transform on hover.
+ * @prop {boolean} disabled - (Optional) If true, disable actions triggering and styles in component.
+ * @prop {function} icon - (Optional) Function that returns an svg icon.
+ * @prop {function} onClick - (Optional) Callback to trigger on onClick event.
+ * @prop {object} children - (Optional) HTML element to be rendered, e.g.: strong tag inside button.
+ */
 export const Solid = () => (
   <>
     <Button label="Primary big button" type="primary-default-large" />
