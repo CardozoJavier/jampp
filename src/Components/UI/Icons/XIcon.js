06f2173899886b3223b6e92d95fdd390aa350662
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2019-12-31 01:25:57 -0300

    Styles fixed

--- src/Components/UI/Icons/XIcon.js
+++ src/Components/UI/Icons/XIcon.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { XIconContainer } from './styles';
 
 export default ({ props }) => (
-  <XIconContainer {...props}>
+  <XIconContainer {...props} role="button">
     <svg viewBox="0 0 4 4" xmlns="http://www.w3.org/2000/svg">
       <path d="M3.53488 0L2 1.53489L0.465116 0L0 0.465119L1.53488 2L0 3.53488L0.465117 4L2 2.46512L3.53488 4L4 3.53488L2.46512 2L4 0.465119L3.53488 0Z" />
     </svg>
