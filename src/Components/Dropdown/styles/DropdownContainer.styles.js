956a85dc859a0122152033b855181550462dbac3
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2020-01-04 12:35:17 -0300

    Prop renamed in dropdownContainer

--- src/Components/Dropdown/styles/DropdownContainer.styles.js
+++ src/Components/Dropdown/styles/DropdownContainer.styles.js
@@ -4,7 +4,7 @@ export const DropdownContainer = styled.div`
   display: flex;
   position: relative;
   flex-direction: ${({ direction }) => direction || ''};
-  align-items: ${({ align }) => align || 'center'};
+  align-items: ${({ alignItems }) => alignItems || 'center'};
   margin: ${({ margin }) => margin};
   padding: ${({ padding }) => padding};
   width: ${({ width }) => width};
