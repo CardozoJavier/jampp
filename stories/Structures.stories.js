ec967ac6e680bd51a2da9ef08f3d7b9f2b4c55e0
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2020-01-04 01:23:18 -0300

    WIP in stories for structures

--- stories/Structures.stories.js
+++ stories/Structures.stories.js
@@ -6,6 +6,8 @@ import {
   Button,
   Checkbox,
   OptionDropdown,
+  FooterBreadcrumb,
+  Step,
 } from '../src/Components';
 import {
   DropdownLabel,
@@ -16,10 +18,13 @@ import {
   ModalContainerA,
   ModalFooter,
   ModalText,
+  ModalContainer,
 } from '../src/Components/Modal/styles';
 import { Option } from '../src/Components/OptionList/styles';
 import { TrashIcon, FilterIcon } from '../src/Components/UI/Icons';
 import { palette } from '../src/Components/styles';
+import { SwitchGroup } from '../src/Components/Switch';
+import { LabelContainer } from '../src/Components/InputField/styles';
 const { gray } = palette;
 
 export default {
@@ -28,109 +33,145 @@ export default {
 
 
 export const ModalA = () => (
-  <Modal title="Modal Title">
-    <ModalRowContainer padding="20px" justifyContent="space-between">
-      <InputField label="Advertiser Name" id='input1' placeholder="Name" type="basic" onChange={value => console.log(value)} />
-      <DropdownContainer width='100%' direction='column' align='start' margin="0 0 0 15px">
-        <DropdownLabel>Vertical</DropdownLabel>
-        <MultipleOptionDropdown text="Select Vertical" type='basic'>
-          <Checkbox label="Option 1" type="dropdown" right />
-          <Checkbox label="Option 2" type="dropdown" right />
-          <Checkbox label="Option 3" type="dropdown" right />
-          <Checkbox label="Option 4" type="dropdown" right />
-        </MultipleOptionDropdown>
-      </DropdownContainer>
-    </ModalRowContainer>
+  <ModalContainer width="600px">
+    <Modal title="Modal Title">
+      <ModalRowContainer padding="0 10px 0 0" padding="20px" justifyContent="space-between">
+        <LabelContainer padding="0 10px 0 0" margin="0 5px 0 0">
+          <InputField label="Advertiser Name" id='input1' placeholder="Name" type="basic" onChange={value => console.log(value)} />
+        </LabelContainer>
+        <DropdownContainer width='100%' direction='column' align='start' margin="0 0 0 15px">
+          <DropdownLabel>Vertical</DropdownLabel>
+          <MultipleOptionDropdown text="Select Vertical" type='basic' />
+        </DropdownContainer>
+      </ModalRowContainer>
 
-    <ModalRowContainer padding="20px" justifyContent="space-between">
-      <DropdownContainer width='100%' direction='column' align='start' margin="0 15px 0 0">
-        <DropdownLabel>Country</DropdownLabel>
-        <OptionDropdown text="Argentina" type='basic'>
-          <Option label="Option 1" id="id1" />
-          <Option label="Option 2" id="id2" />
-          <Option label="Option 3" id="id3" />
-          <Option label="Option 4" id="id4" />
-        </OptionDropdown>
-      </DropdownContainer>
-      <DropdownContainer width='100%' direction='column' align='start' margin="0 0 0 15px">
-        <DropdownLabel>Region</DropdownLabel>
-        <MultipleOptionDropdown text="Select Region" type='basic'>
-          <Checkbox label="Option 1" type="dropdown" right />
-          <Checkbox label="Option 2" type="dropdown" right />
-          <Checkbox label="Option 3" type="dropdown" right />
-          <Checkbox label="Option 4" type="dropdown" right />
-        </MultipleOptionDropdown>
-      </DropdownContainer>
-    </ModalRowContainer>
+      <ModalRowContainer padding="20px" justifyContent="space-between">
+        <DropdownContainer width='100%' direction='column' align='start' margin="0 15px 0 0">
+          <DropdownLabel>Country</DropdownLabel>
+          <OptionDropdown text="Argentina" type='basic' />
+        </DropdownContainer>
+        <DropdownContainer width='100%' direction='column' align='start' margin="0 0 0 15px">
+          <DropdownLabel>Region</DropdownLabel>
+          <MultipleOptionDropdown text="Select Region" type='basic' />
+        </DropdownContainer>
+      </ModalRowContainer>
 
 
-    <ModalRowContainer padding="20px" justifyContent="space-between">
-      <DropdownContainer width='47.5%' direction='column' align='start' margin="0 15px 0 0">
-        <DropdownLabel>Domain</DropdownLabel>
-        <MultipleOptionDropdown text="http://www.yourdomain.com/" type='basic'>
-          <Checkbox label="Option 1" type="dropdown" right />
-          <Checkbox label="Option 2" type="dropdown" right />
-          <Checkbox label="Option 3" type="dropdown" right />
-          <Checkbox label="Option 4" type="dropdown" right />
-        </MultipleOptionDropdown>
-      </DropdownContainer>
-    </ModalRowContainer>
+      <ModalRowContainer padding="20px" justifyContent="space-between">
+        <DropdownContainer width='47.5%' direction='column' align='start' margin="0 15px 0 0">
+          <DropdownLabel>Domain</DropdownLabel>
+          <MultipleOptionDropdown text="http://www.yourdomain.com/" type='basic'>
+            <Checkbox label="Option 1" type="dropdown" right />
+            <Checkbox label="Option 2" type="dropdown" right />
+            <Checkbox label="Option 3" type="dropdown" right />
+            <Checkbox label="Option 4" type="dropdown" right />
+          </MultipleOptionDropdown>
+        </DropdownContainer>
+      </ModalRowContainer>
 
-    <ModalFooter>
-      <Button label="Action button" type="secondary-default-medium" />
-    </ModalFooter>
-  </Modal>
+      <ModalFooter>
+        <Button label="Action button" type="secondary-default-medium" />
+      </ModalFooter>
+    </Modal>
+  </ModalContainer>
 );
 
 export const ModalB = () => (
-  <ModalContainerA>
-    <Modal title="Modal Title" icon={FilterIcon}>
-      <>
-      <ModalText>
-        Question that needs to be answered with bottons below?
-      </ModalText>
-      <ModalRowContainer>
-        <Button label="No, cancel" type="secondary-gray-medium"/>
-        <Button label="Yes, delete" type="secondary-default-medium"/>
-      </ModalRowContainer>
-      </>
-    </Modal>
-  </ModalContainerA>
+  <ModalContainer width="600px" minHeight="0">
+    <ModalContainerA>
+      <Modal title="Modal Title" icon={FilterIcon}>
+        <>
+        <ModalText>
+          Question that needs to be answered with bottons below?
+        </ModalText>
+        <ModalRowContainer>
+          <Button label="No, cancel" type="secondary-gray-medium"/>
+          <Button label="Yes, delete" type="secondary-default-medium"/>
+        </ModalRowContainer>
+        </>
+      </Modal>
+    </ModalContainerA>
+  </ModalContainer>
 );
 
 export const ModalC = () => (
-  <Modal title="Modal Title">
-    <ModalRowContainer padding='0 20px' justifyContent="space-between">
-      <DropdownContainer width='50%' direction='column' align='start'>
-        <DropdownLabel>Vertical</DropdownLabel>
-        <MultipleOptionDropdown text="Select Vertical" type='basic' />
-      </DropdownContainer>
-    </ModalRowContainer>
+  <ModalContainer width="600px">
+    <Modal title="Modal Title">
+      <ModalRowContainer padding='0 20px' justifyContent="space-between">
+        <DropdownContainer width='50%' direction='column' align='start'>
+          <DropdownLabel>Vertical</DropdownLabel>
+          <MultipleOptionDropdown text="Select Vertical" type='basic' />
+        </DropdownContainer>
+      </ModalRowContainer>
 
-    <ModalRowContainer padding='0 20px' justifyContent="space-between">
-      <ModalText fontSize='10px' color={gray.g3} margin='16px 0 32px 0'>
-        With monthly invoicing, you get an invoice for each ad account on the 1st of each month, 
-        and then you’d have to pay the invoices by their due date. 
-        A line of credits is best for our largest advertisers adn can take several days to approve. 
-        If you want to spend now, a credit card is best option.
-      </ModalText>
-    </ModalRowContainer>
+      <ModalRowContainer padding='0 20px' justifyContent="space-between">
+        <ModalText fontSize='10px' color={gray.g3} margin='16px 0 32px 0'>
+          With monthly invoicing, you get an invoice for each ad account on the 1st of each month, 
+          and then you’d have to pay the invoices by their due date. 
+          A line of credits is best for our largest advertisers adn can take several days to approve. 
+          If you want to spend now, a credit card is best option.
+        </ModalText>
+      </ModalRowContainer>
 
 
-    <ModalRowContainer padding='0 20px' justifyContent="flex-start">
-      <DropdownContainer width='50%' direction='column' align='start'>
-        <DropdownLabel>Domain</DropdownLabel>
-        <MultipleOptionDropdown text="http://www.yourdomain.com/" type='basic' />
-      </DropdownContainer>
-      <DropdownContainer width='25%' direction='column' align='start' margin="0 0 0 40px">
-        <DropdownLabel>Domain</DropdownLabel>
-        <MultipleOptionDropdown text="Text" type='basic' />
-      </DropdownContainer>
-    </ModalRowContainer>
+      <ModalRowContainer padding='0 20px 20px 20px' justifyContent="flex-start">
+        <DropdownContainer width='50%' direction='column' align='start'>
+          <DropdownLabel>Domain</DropdownLabel>
+          <MultipleOptionDropdown text="http://www.yourdomain.com/" type='basic' />
+        </DropdownContainer>
+        <DropdownContainer width='25%' direction='column' align='start' margin="0 0 0 40px">
+          <DropdownLabel>Domain</DropdownLabel>
+          <MultipleOptionDropdown text="Text" type='basic' />
+        </DropdownContainer>
+      </ModalRowContainer>
 
-    <ModalFooter>
-      <Button label="Action button" type="secondary-default-medium" />
-    </ModalFooter>
-  </Modal>
+      <ModalFooter>
+        <Button label="Action button" type="secondary-default-medium" />
+      </ModalFooter>
+    </Modal>
+  </ModalContainer>
 );
 
+export const ModalD = () => (
+  <div style={{ display: 'flex', flexDirection: 'column', justifyContent: "space-between", height: '100vh', width: '100%', alignItems: 'center', }}>
+    <ModalContainer width="920px">
+      <Modal title="Title" type="">
+        <ModalRowContainer padding="20px" justifyContent="">
+          <LabelContainer width="50%" margin="0 40px 0 0">
+            <InputField label="Advertiser Name" id='input1' placeholder="Name" type="basic" onChange={value => console.log(value)} />
+          </LabelContainer>
+          <SwitchGroup>
+            <Option label="Choice" id="id1" />
+            <Option label="Choice" id="id2" />
+          </SwitchGroup>
+        </ModalRowContainer>
+        <ModalRowContainer padding='0 20px 20px 20px' justifyContent="flex-start">
+          <DropdownContainer width='50%' direction='column' align='start'>
+            <DropdownLabel>Domain</DropdownLabel>
+            <MultipleOptionDropdown text="http://www.yourdomain.com/" type='basic' />
+          </DropdownContainer>
+          <DropdownContainer width='17%' direction='column' align='start' margin="0 0 0 40px">
+            <DropdownLabel>Domain</DropdownLabel>
+            <MultipleOptionDropdown text="Text" type='basic' />
+          </DropdownContainer>
+        </ModalRowContainer>
+        <ModalRowContainer padding="20px 20px 40px 20px" justifyContent="space-between">
+          <DropdownContainer width='50%' direction='column' align='start' margin="0">
+            <DropdownLabel>Domain</DropdownLabel>
+            <MultipleOptionDropdown text="http://www.yourdomain.com/" type='basic'>
+              <Checkbox label="Option 1" type="dropdown" right />
+              <Checkbox label="Option 2" type="dropdown" right />
+              <Checkbox label="Option 3" type="dropdown" right />
+              <Checkbox label="Option 4" type="dropdown" right />
+            </MultipleOptionDropdown>
+          </DropdownContainer>
+        </ModalRowContainer>
+      </Modal>
+    </ModalContainer>
+
+    <FooterBreadcrumb>
+      <Step text="1. Step 1" />
+      <Step text="2. Step 2" />
+    </FooterBreadcrumb>
+  </div>
+);
