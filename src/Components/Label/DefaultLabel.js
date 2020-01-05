2e1f3dc4791b7b7d99a04cb074eb3acee50de186
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2020-01-04 22:16:07 -0300

    PropTypes added in DefaultLabel component

--- src/Components/Label/DefaultLabel.js
+++ src/Components/Label/DefaultLabel.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Button } from '../Button';
 import { XIcon } from '../UI/Icons';
 
@@ -8,8 +9,19 @@ import { XIcon } from '../UI/Icons';
  * @param {String} size - (Optional) Define label size, it can be small or medium. Small is default.
  * @param {Function} onClose - (Optional) Callback to trigger on onClick event on X icon.
  */
-const DefaultLabel = ({ text, size = 'small', onClose }) => (
+const DefaultLabel = ({ text, size, onClose }) => (
   <Button label={text} type={`label-default-${size}`} icon={XIcon} onClickIcon={onClose} />
 );
+
+DefaultLabel.propTypes = {
+  text: PropTypes.string.isRequired,
+  size: PropTypes.string,
+  onClose: PropTypes.func,
+};
+
+DefaultLabel.defaultProps = {
+  size: 'small',
+  onClose: () => null,
+};
 
 export default DefaultLabel;
