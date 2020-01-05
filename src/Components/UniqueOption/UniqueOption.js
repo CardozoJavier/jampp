54225553773451e105ad6fbac8f99f03ef5f620b
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2020-01-05 16:38:38 -0300

    PropTypes added in UniqueOption component

--- src/Components/UniqueOption/UniqueOption.js
+++ src/Components/UniqueOption/UniqueOption.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { IconGenerator, CheckIcon } from '../UI/Icons';
 import { bemDestruct } from '../../utils';
 import { OptionContainer, OptionLabel } from './styles';
@@ -9,12 +10,12 @@ const { white } = palette;
 /**
  * UniqueOption component should be called with
  * @param {String} id - (Required) It's an unique ID to identifier each option in options group.
- * @param {String} label - (Optional) It's a text to be displayed in option.
  * @param {String} className - (Required) The className determines option styles.
  * @param {Function} handleCheck - (Required) It's a callback to be called in onClick event.
+ * @param {String} label - (Optional) It's a text to be displayed in option.
  * @return {React Component} A view for label and selectable option.
  */
-const UniqueOption = ({ id, label, className, handleCheck }) => (
+const UniqueOption = ({ id, className, handleCheck, label, }) => (
   <OptionContainer
     key={id}
     className={bemDestruct(className)}
@@ -33,5 +34,16 @@ const UniqueOption = ({ id, label, className, handleCheck }) => (
     />
   </OptionContainer>
 )
+
+UniqueOption.propTypes = {
+  id: PropTypes.string.isRequired,
+  className: PropTypes.string.isRequired,
+  handleCheck: PropTypes.func.isRequired,
+  label: PropTypes.string,
+};
+
+UniqueOption.defaultProps = {
+  label: '',
+};
 
 export default UniqueOption;
