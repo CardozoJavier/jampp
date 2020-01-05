1dafce3092b95281ca5ed7de6f55a6fdf9e75c35
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2020-01-04 22:13:05 -0300

    PropTypes added in Checkbox component

--- src/Components/Checkbox/Checkbox.js
+++ src/Components/Checkbox/Checkbox.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import { IconGenerator } from '../UI/Icons';
 import { bemDestruct, getClassName, } from '../../utils';
 import { CheckboxContainer, CheckboxLabel, Box, Container } from './styles';
@@ -26,14 +27,14 @@ const classesName = {
  *  @param {String} id - (Optional) It's an unique ID to identifier each checkbox.
  *  @param {String} label - (Optional) It's a text to be display next to checkbox.
  *  @param {String} type - (Required) Define dropdown classes for styling.
- *  @param {Boolean} right - (Required) It's required if left prop is not set. It's define the position of checkbox regarding text.
- *  @param {Boolean} left - (Required) It's required if right prop is not set. It's define the position of checkbox regarding text.
+ *  @param {Boolean} right - (Optional) It's required if left prop is not set. It's define the position of checkbox regarding text.
+ *  @param {Boolean} left - (Optional) It's required if right prop is not set. It's define the position of checkbox regarding text.
  *  @param {Function} onChange - (Optional) Callback to trigger on onChange event. It receive two arguments: checkbox id and true/false value.
  *  @param {Boolean} checked - (Optional) It's determines if checkbox is checked or not by default.
  *  @param {Boolean} disabled - (Optional) If true, disable actions triggering and styles in component.
  *  @return {React Component} A view for checkbox.
  */
-const Checkbox = ({ id, label, type = 'normal', right, left, onChange, checked = false, disabled }) => {
+const Checkbox = ({ id, label, type, right, left, onChange, checked, disabled }) => {
   const { defaultClassName, optionalClassName } = classesName[type];
   const { defaultLabel, selectedLabel } = classesName['label'];
   const initialState = checked ? optionalClassName : defaultClassName;
@@ -54,7 +55,7 @@ const Checkbox = ({ id, label, type = 'normal', right, left, onChange, checked =
 
   return (
     <Container className={bemDestruct(className, disabled)} onClick={disabled ? null : handleClick} disabled={disabled}>
-      { right && <CheckboxLabel className={bemDestruct(labelClassName)} disabled={disabled}>{ label }</CheckboxLabel>}
+      {right && !left && <CheckboxLabel className={bemDestruct(labelClassName)} disabled={disabled}>{ label }</CheckboxLabel>}
       <CheckboxContainer>
         <Box className={bemDestruct(className, disabled)} />
         <IconGenerator
@@ -68,9 +69,31 @@ const Checkbox = ({ id, label, type = 'normal', right, left, onChange, checked =
           }}
         />
       </CheckboxContainer>
-      { left && <CheckboxLabel className={bemDestruct(labelClassName)} disabled={disabled}>{ label }</CheckboxLabel>}
+      { left && !right && <CheckboxLabel className={bemDestruct(labelClassName)} disabled={disabled}>{ label }</CheckboxLabel>}
     </Container>
   )
+};
+
+Checkbox.propTypes = {
+  id: PropTypes.string,
+  label: PropTypes.string,
+  type: PropTypes.string,
+  right: PropTypes.bool,
+  left: PropTypes.bool,
+  onChange: PropTypes.func,
+  checked: PropTypes.bool,
+  disabled: PropTypes.bool,
+};
+
+Checkbox.defaultProps = {
+  id: '',
+  label: '',
+  type: 'normal',
+  right: false,
+  left: false,
+  onChange: () => null,
+  checked: false,
+  disabled: false,
 };
 
 export default Checkbox;
