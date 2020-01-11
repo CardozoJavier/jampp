fd2773e361002ed80bb29c57e9a2125fc2873e7b
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2020-01-11 12:38:26 -0300

    Container for StatusLabel moved from button to Label component

--- src/Components/Button/styles/index.js
+++ src/Components/Button/styles/index.js
@@ -1,7 +1,6 @@
 export { default as ButtonInput } from './ButtonInput.styles';
 export { default as LinkButtonContainer } from './LinkButtonContainer.styles';
 export { default as OptionButtonContainer } from './OptionButtonContainer.styles';
-export { default as StatusButtonContainer } from './StatusButtonContainer.styles';
 export { default as PrimaryButtonContainer } from './PrimaryButtonContainer.styles';
 export { default as SecondaryButtonContainer } from './SecondaryButtonContainer.styles';
 export { default as AccessoryButtonContainer } from './AccessoryButtonContainer.styles';
