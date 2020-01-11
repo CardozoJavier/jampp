2696e40f7b6f0fd90ed7beb3b621624cc1e9c5a9
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2020-01-11 19:17:49 -0300

    Table  component created

---
+++ src/Components/Table/Table.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { TableContainer, ColumnTitle } from './styles';
+
+const Table = () => (
+  <TableContainer>
+    <ColumnTitle></ColumnTitle>
+  </TableContainer>
+);
+
+export default Table;
