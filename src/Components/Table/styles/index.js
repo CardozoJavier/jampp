7071b4a6adbd71b34ed5e6441a2ad639595db3b3
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2020-01-12 18:16:19 -0300

    Styles for Table created

--- src/Components/Table/styles/index.js
+++ src/Components/Table/styles/index.js
@@ -1,7 +1,12 @@
 export {
+  Row,
+  RowName,
+  RowTitle,
   TableTitle,
+  ColumnField,
   TableHeader,
   ColumnTitle,
   TableButtons,
   TableContainer,
+  ColumnFieldButton,
 } from './Table.styles';
