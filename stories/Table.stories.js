e91c45d1e469f29df4fc8ccca673612d9f9b2c8d
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2020-01-12 18:16:34 -0300

    Stories for Table created

--- stories/Table.stories.js
+++ stories/Table.stories.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { Card, InputField, Button, OptionDropdown } from '../src/Components';
+import { Table } from '../src/Components/Table';
 import {
-  Table,
-  TableItem,
-} from '../src/Components/Table';
-import {
+  Row,
+  RowName,
+  RowTitle,
   TableTitle,
-  TableButtons,
+  ColumnField,
   ColumnTitle,
+  TableButtons,
+  ColumnFieldButton,
 } from '../src/Components/Table/styles';
 import {
   CardHeader,
 } from '../src/Components/Card/styles';
-import { CardContainer } from '../src/Components/Card/styles';
 import { SearchIcon, DownloadFileIcon, AddIcon } from '../src/Components/UI/Icons';
 import { SearchBarContainer } from '../src/Components/InputField/styles';
 
@@ -21,13 +22,12 @@ export default {
 };
 
 /**
- * Table component can be called with
- * @prop {String} title - (Optional) It's title to be displayed in card.
- * @prop {String} description - (Optional) It's description to be displayed in card below of title.
+ * Table component should be called with
+ * @prop {Node} children - (Required) It's the content to be displayed.
  */
 export const WithTitle = () => (
-  <CardContainer width="1392px">
-    <CardHeader borderBottom="none" flexDirection="row">
+  <Card width="1392px" padding="16px 16px 62px 16px">
+    <CardHeader borderBottom="none">
       <TableTitle>{ "Table Title" }</TableTitle>
       <SearchBarContainer width="535px">
         <InputField placeholder="Text" type="searchbar" icon={SearchIcon} />
@@ -37,10 +37,69 @@ export const WithTitle = () => (
         <Button label="CTA text" type="accessory-default-left" icon={AddIcon} />
       </TableButtons>
     </CardHeader>
-    <Card>
-      <Table>
-        <ColumnTitle>{ '' }</ColumnTitle>
-      </Table>
-    </Card>
-  </CardContainer>
+    <Table>
+      <RowTitle>
+        <ColumnTitle borderLeft="none">{ 'First column title' }</ColumnTitle>
+        <ColumnTitle>{ 'Column title' }</ColumnTitle>
+        <ColumnTitle>{ 'Column title' }</ColumnTitle>
+        <ColumnTitle>{ 'Column title' }</ColumnTitle>
+        <ColumnTitle>{ 'Column title' }</ColumnTitle>
+      </RowTitle>
+      <Row>
+        <RowName>{ 'Name here' }</RowName>
+        <ColumnField>{ 'Data here' }</ColumnField>
+        <ColumnField>{ 'Data here' }</ColumnField>
+        <ColumnField>{ 'Data here' }</ColumnField>
+        <ColumnField>{ 'Data here' }</ColumnField>
+        <ColumnFieldButton borderLeft="none">
+          <Button label="Button text" type="secondary-gray-medium" />
+          <Button label="Button text" type="secondary-gray-medium" />
+        </ColumnFieldButton>
+      </Row>
+      <Row>
+        <RowName>{ 'Name here' }</RowName>
+        <ColumnField>{ 'Data here' }</ColumnField>
+        <ColumnField>{ 'Data here' }</ColumnField>
+        <ColumnField>{ 'Data here' }</ColumnField>
+        <ColumnField>{ 'Data here' }</ColumnField>
+        <ColumnFieldButton borderLeft="none">
+          <Button label="Button text" type="secondary-gray-medium" />
+          <Button label="Button text" type="secondary-gray-medium" />
+        </ColumnFieldButton>
+      </Row>
+      <Row>
+        <RowName>{ 'Name here' }</RowName>
+        <ColumnField>{ 'Data here' }</ColumnField>
+        <ColumnField>{ 'Data here' }</ColumnField>
+        <ColumnField>{ 'Data here' }</ColumnField>
+        <ColumnField>{ 'Data here' }</ColumnField>
+        <ColumnFieldButton borderLeft="none">
+          <Button label="Button text" type="secondary-gray-medium" />
+          <Button label="Button text" type="secondary-gray-medium" />
+        </ColumnFieldButton>
+      </Row>
+      <Row>
+        <RowName>{ 'Name here' }</RowName>
+        <ColumnField>{ 'Data here' }</ColumnField>
+        <ColumnField>{ 'Data here' }</ColumnField>
+        <ColumnField>{ 'Data here' }</ColumnField>
+        <ColumnField>{ 'Data here' }</ColumnField>
+        <ColumnFieldButton borderLeft="none">
+          <Button label="Button text" type="secondary-gray-medium" />
+          <Button label="Button text" type="secondary-gray-medium" />
+        </ColumnFieldButton>
+      </Row>
+      <Row>
+        <RowName>{ 'Name here' }</RowName>
+        <ColumnField>{ 'Data here' }</ColumnField>
+        <ColumnField>{ 'Data here' }</ColumnField>
+        <ColumnField>{ 'Data here' }</ColumnField>
+        <ColumnField>{ 'Data here' }</ColumnField>
+        <ColumnFieldButton borderLeft="none">
+          <Button label="Button text" type="secondary-gray-medium" />
+          <Button label="Button text" type="secondary-gray-medium" />
+        </ColumnFieldButton>
+      </Row>
+    </Table>
+  </Card>
 );
