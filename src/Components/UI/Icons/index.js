c7cbaebcbbbcc7686657f34ecc2a0a600c2e4102
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2020-01-14 23:42:57 -0300

    Always trigger error in input field

--- src/Components/UI/Icons/index.js
+++ src/Components/UI/Icons/index.js
@@ -4,6 +4,7 @@ export { default as ViewIcon } from './ViewIcon';
 export { default as EditIcon } from './EditIcon';
 export { default as HelpIcon } from './HelpIcon';
 export { default as InfoIcon } from './InfoIcon';
+export { default as AlertIcon } from './AlertIcon';
 export { default as CheckIcon } from './CheckIcon';
 export { default as TrashIcon } from './TrashIcon';
 export { default as ArrowIcon } from './ArrowIcon';
