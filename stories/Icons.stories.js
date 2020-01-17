9f738592fdf5cc6e3e22cb0a8b5bd8b998f99f3c
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2020-01-16 23:11:51 -0300

    Icons added in stories

--- stories/Icons.stories.js
+++ stories/Icons.stories.js
@@ -1,6 +1,27 @@
 import React from 'react';
-import { CalendarIcon, SearchIcon, FilterIcon, ViewIcon, CreditCardIcon, EditIcon, AddIcon, ArrowIcon, BillingIcon, CheckIcon, DownChevronIcon, DownloadFileIcon, ExclamationIcon, HelpIcon, InfoIcon, NotificationsIcon, SuccessIcon, TrashIcon, XIcon } from '../src/Components/UI/Icons';
-import AddAccountIcon from '../src/Components/UI/Icons/AddAccountIcon';
+import {
+  CalendarIcon,
+  SearchIcon,
+  FilterIcon,
+  ViewIcon,
+  CreditCardIcon,
+  EditIcon,
+  AddIcon,
+  ArrowIcon,
+  BillingIcon,
+  CheckIcon,
+  DownChevronIcon,
+  DownloadFileIcon,
+  ExclamationIcon,
+  HelpIcon,
+  InfoIcon,
+  NotificationsIcon,
+  SuccessIcon,
+  TrashIcon,
+  XIcon,
+  AlertIcon,
+  AddAccountIcon
+} from '../src/Components/UI/Icons';
 
 export default {
   title: 'Icons',
@@ -25,7 +46,9 @@ export const Icons = () => (
     <NotificationsIcon props={{ position: 'unset', height: '18px', width: '18px', fill: '#000' }} />
     <SuccessIcon props={{ position: 'unset', height: '18px', width: '18px', fill: '#000' }} />
     <TrashIcon props={{ position: 'unset', height: '18px', width: '18px', fill: '#000' }} />
-    <XIcon props={{ position: 'unset', height: '18px', width: '18px', fill: '#000' }} />
+    <XIcon props={{ position: 'unset', height: '14px', width: '14px', fill: '#000' }} />
+    <AlertIcon props={{ height: '18px', width: '18px', fill: '#000' }} />
+    <ExclamationIcon props={{ height: '18px', width: '18px', fill: '#000' }} />
   </>
 );
 
