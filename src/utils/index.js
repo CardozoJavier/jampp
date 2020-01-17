50e18f41ba69009631dfa9b4df8f54834bfcdfcd
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2020-01-16 23:14:24 -0300

    Hook to generate an unique id created

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,3 +4,5 @@ export { default as dropdownAction } from './dropdownAction';
 export { default as getPosition } from './getPosition';
 export { default as settingClassName } from './settingClassName';
 export { default as isLastItem } from './isLastItem';
+export { default as useEventListener } from './useEventListener';
+export { default as getUniqueId } from './getUniqueId';
