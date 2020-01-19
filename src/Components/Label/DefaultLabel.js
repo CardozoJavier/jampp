0e27819d878555c617bf69f75a8e61ffad0b3ead
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2020-01-19 19:27:05 -0300

    DefaultLabel component refactored to support onClose event

--- src/Components/Label/DefaultLabel.js
+++ src/Components/Label/DefaultLabel.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { DefaultLabelContainer, DefaultLabelText } from './styles';
 import { XIcon, IconGenerator } from '../UI/Icons';
@@ -10,18 +10,28 @@ import { bemDestruct } from '../../utils';
  * @param {String} size - (Optional) Define label size, it can be small or medium. Small is default.
  * @param {Function} onClose - (Optional) Callback to trigger on onClick event on X icon.
  */
-const DefaultLabel = ({ text, size, onClose }) => (
-  <DefaultLabelContainer className={bemDestruct(`label label--default-${size}`)}>
-    <DefaultLabelText>{ text }</DefaultLabelText>
-    <IconGenerator
-      renderIcon={XIcon}
-      props={{
-        className: `icon label--default-${size}__right`,
-        onClick: onClose,
-      }}
-    />
-  </DefaultLabelContainer>
-);
+const DefaultLabel = ({ text, size, onClose, ...props }) => {
+  const [className, setClassName] = useState(`label label--default-${size}`);
+
+  const handleClick = () => {
+    onClose && onClose(text);
+    setClassName(`label label--default-${size}__closed`);
+  };
+
+  return (
+    <DefaultLabelContainer className={bemDestruct(className)} {...props}>
+      <DefaultLabelText {...props}>{ text }</DefaultLabelText>
+      <IconGenerator
+        renderIcon={XIcon}
+        props={{
+          className: `icon label--default-${size}__right`,
+          onClick: handleClick,
+          display: '',
+        }}
+      />
+    </DefaultLabelContainer>
+  );
+};
 
 DefaultLabel.propTypes = {
   text: PropTypes.string.isRequired,
