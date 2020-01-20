58c3fa2692730fa43b432eeb805f9e49dff9bffa
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2020-01-20 20:24:45 -0300

    Row component convert from styled component to functional component to support loading state

--- src/Components/Table/index.js
+++ src/Components/Table/index.js
@@ -1,4 +1,5 @@
+export { default as Row } from './Row';
 export { default as Table } from './Table';
 export { default as TableItem } from './TableItem';
-export { default as PaymentMethodTable } from './PaymentMethodTable';
 export { default as PaymentRow } from './PaymentRow';
+export { default as PaymentMethodTable } from './PaymentMethodTable';
