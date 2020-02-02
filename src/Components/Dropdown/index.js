f646899e4dd1abfb4005e6b45d8f69bbfc3983c1
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2020-02-02 16:02:29 -0300

    StatusLabelDropdown deleted

--- src/Components/Dropdown/index.js
+++ src/Components/Dropdown/index.js
@@ -2,7 +2,6 @@ export { default as OptionDropdown } from './OptionDropdown';
 export { default as AccountDropdown } from './AccountDropdown';
 export { default as AdAccountsDropdown } from './AdAccountsDropdown';
 export { default as ExpandableDropdown } from './ExpandableDropdown';
-export { default as StatusLabelDropdown } from './StatusLabelDropdown';
 export { default as HeaderOptionDropdown } from './HeaderOptionDropdown';
 export { default as MultipleOptionDropdown } from './MultipleOptionDropdown';
 export { default as AdditionalDetailsDropdown } from './AdditionalDetailsDropdown'; 
--- src/Components/index.js
+++ src/Components/index.js
@@ -17,7 +17,6 @@ export {
   OptionDropdown,
   AccountDropdown,
   AdAccountsDropdown,
-  StatusLabelDropdown,
   HeaderOptionDropdown,
   MultipleOptionDropdown,
   AdditionalDetailsDropdown,
