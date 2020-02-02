0b98653fa460c4d41b175d74b1576ba0d966160b
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2020-02-01 21:35:16 -0300

    Global fonts variables created

---
+++ src/Components/styles/fonts.styles.js
@@ -0,0 +1,12 @@
+/**
+ * Define common styles for fonts.
+ */
+const fonts = {
+  size10: '10px',
+  size12: '12px',
+  size14: '14px',
+  size16: '16px',
+  size18: '18px',
+};
+
+export default fonts;
