9b453e71fe6a715a9b711e70d1acd6c51cdec294
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2020-02-03 09:12:01 -0300

    Fix linting

--- src/Components/Dropdown/styles/DropdownListContainer.styles.js
+++ src/Components/Dropdown/styles/DropdownListContainer.styles.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import { OptionCheckboxGroup } from '../../OptionList/styles';
 
 export const DropdownListContainer = styled.div`
   width: ${({ width }) => width || '100%'};
