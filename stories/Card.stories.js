7a587fa9260b2fd40f2de104cf41fe3972fae18d
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2020-02-08 15:04:18 -0300

    Stories for expandable card created

--- stories/Card.stories.js
+++ stories/Card.stories.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Card } from '../src/Components';
-import { CardContainer } from '../src/Components/Card/styles';
+import { ExpandableCard } from '../src/Components/Card';
 
 export default {
   title: 'Cards',
@@ -10,7 +10,7 @@ export default {
  * Card component can be called with
  * @prop {String} title - (Optional) It's title to be displayed in card.
  * @prop {String} description - (Optional) It's description to be displayed in card below of title.
- * @prop {String} width - (Optional) It's the width of the card container.
+ * @prop {String} width - (Optional) It's the width of the card container. Default value is 600px.
  */
 export const WithTitle = () => (
   <Card title="Title" />
@@ -21,4 +21,16 @@ export const WithTitleAndDescription = () => (
     title="Title"
     description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
   />
+);
+
+/**
+ * ExpandableCard component can be called with
+ * @prop {Node} children - (Optional)  It's the content to be displayed into Card.
+ * @prop {String} title - (Optional) It's title to be displayed in card.
+ * @prop {String} description - (Optional) It's description to be displayed in card below of title.
+ * @prop {String} width - (Optional) It's the width of the card container. Default value is 600px.
+ * @prop {String} padding - (Optional) It's the padding of the card container.
+ */
+export const Expandable = () => (
+  <ExpandableCard width="800px" title="Title" description="Short description of content" />
 );
