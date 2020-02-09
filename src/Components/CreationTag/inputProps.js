c542aa1ba83154ef19ba4170861c44e8943a133d
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2020-02-09 14:45:24 -0300

    CreationTagSuggestion component created

--- src/Components/CreationTag/inputProps.js
+++ src/Components/CreationTag/inputProps.js
@@ -11,6 +11,13 @@ const inputProps = {
     errorClassName: "",
     InputContainer: CreationTagContainer,
   },
+  "suggestions-default": {
+    defaultClassName: 'input basic--default-medium',
+    onFocusClassName: "input basic--default-medium__focus",
+    onBlurClassName: "input basic--default-medium__blur",
+    errorClassName: "",
+    InputContainer: CreationTagContainer,
+  },
 };
 
 export default inputProps;
