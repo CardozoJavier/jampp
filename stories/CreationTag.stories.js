ba9ebc053e8b6c09f9c1cb0d23b7b42723305dc4
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2020-02-11 19:26:48 -0300

    Stories for CreationTracking created

--- stories/CreationTag.stories.js
+++ stories/CreationTag.stories.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../src/main.css';
-import { CreationTag, CreationTagSuggestion } from '../src/Components/CreationTag';
+import { CreationTag, CreationTagSuggestion, CreationTracking } from '../src/Components/CreationTag';
 export default {
   title: 'Creation tag'
 };
@@ -42,6 +42,29 @@ export const Default = () => (
 export const Suggestions = () => (
   <CreationTagSuggestion
     type="suggestions-default"
+    width="433px"
+    suggestions={["Option 1", "Option 2", "Option 3"]}
+    callback={() => console.log('Displaying full list')}
+    onTagCreated={event => console.log(event + ' has been created')}
+    onTagDeleted={event => console.log(event + ' has been deleted')}
+  />
+);
+
+/**
+ * CreationTagSuggestion component should be called with
+ * @prop {String} type - (Required) It's to define styles of input field.
+ * @prop {String} placeholder - (Optional) It's to display text into input field.
+ * @prop {Boolean} disabled - (Optional) If true, disable actions triggering and styles in component.
+ * @prop {String} label - (Optional) Text to be display in label.
+ * @prop {String} id - (Optional) ID to be use for label refering to input field.
+ * @prop {Function} onTagCreated - (Optional) Callback to trigger on tag created. It receive tag value in first argument.
+ * @prop {Function} onTagDeleted - (Optional) Callback to trigger when tag is deleted. It receive tag value in first argument.
+ * @prop {Array} suggestions - (Required) Array of string to be displayed in suggestions list.
+ * @prop {Function} callback - (Optional) Callback to be triggered on click event in button into suggestions list.
+ */
+export const TrackingSuggestions = () => (
+  <CreationTracking
+    type="suggestions-tracking"
     width="433px"
     suggestions={["Option 1", "Option 2", "Option 3"]}
     callback={() => console.log('Displaying full list')}
