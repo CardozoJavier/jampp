9973cfdbdda4e33f9f9d0828b24f5d59c7dc7be8
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2020-02-15 21:34:57 -0300

    CloneElement refacfored to support any type of structure and remove structure event

--- src/Components/CloneElement/CloneElement.js
+++ src/Components/CloneElement/CloneElement.js
@@ -1,40 +1,63 @@
 import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import { CloneElementContainer } from './styles';
 import { Button } from '../Button';
 
 /**
  *  CloneElement component should be called with
- *  @param {Array} children - (Required) They're the structures to be cloned. Can be one React node or an array of React nodes.
+ *  @param {Array} children - (Required) They're the structures to be cloned. Should be one React node.
+ *  @param {String} buttonText - (Optional) The text to be displayed into button add structure. Default value is empty string.
+ *  @param {String} buttonType - (Optional) The button type corresponding to class styles. Default value is link-default-left.
+ *  @param {Node} buttonIcon - (Optional) The icon to be displayed into button. Default value is 'null'.
+ *  @param {Boolean} removableFirst - (Optional) Is the flag to determine if the first structure is removable or not. Default value is true.
+ *  @param {Object} buttonProps - (Optional) It's the props to be passed to structure container for modifying Add button styles.
+ *  @param {Function} onDeleteCallback - (Required) It's the callback to be called when remove icon is clicked. It receive the structure ID in first argument.
  */
-const CloneElement = ({ children, buttonText, buttonType, buttonIcon, removableFirst, iconRemove }) => {
-  const elementCloned = React.cloneElement(children);
-  const initialState = Array.isArray(children) ? [...children] : [children];
+const CloneElement = ({ children, buttonText, buttonType, buttonIcon, removableFirst, buttonProps, onDeleteCallback }) => {
+  const initialState = Object.assign({}, children);
   const [structure, setStructure] = useState(initialState);
 
   /**
-   * Modify 'id' prop from origin children element.
+   * Modifying onClick callback to receive the ID of each cloned structure to be removed.
    */
-  const settingUniqueStructureId = (clone) => (
-    clone.map(child => (
-      React.cloneElement(child, {
-        id: Math.random().toString(),
-        key: Math.random().toString(),
+  const settingOnClickCallback = (arrayChildren, id) => (
+    arrayChildren.map(child => {
+      const key = Math.random().toString();
+      if (child.props.role === 'icon-to-remove-structure') {
+        return React.cloneElement(child, {
+          key,
+          props: {
+            ...child.props.props,
+            onClick: () => onDeleteCallback(id),
+          }
+        });
+      } else {
+        return React.cloneElement(child, { key });
+      }
+    })
+  ); 
+
+  /**
+   * Assigning unique id to new structures
+   */
+  const cloningWithUniqueId = (clone) => {
+    const key = Math.random().toString();
+    return (
+      React.cloneElement(clone, {
+        key: key,
+        id: key,
+        className: 'trash-icon__visible',
+        children: clone.props.children ? settingOnClickCallback(clone.props.children, key) : null,
       })
-    ))
-  );
+    );
+  };
 
   /**
    * Add duplicated children structure to be rendered.
-   */ 
+   */
   const handleAddStructure = () => {
-    const key = Math.random().toString();
     const structuresArray = Array.isArray(structure) ? [...structure] : [structure];
-    const newStructure = Array.isArray(initialState) ? [...initialState] : React.cloneElement(elementCloned, {
-      key,
-      id: key,
-      className: 'trash-icon__visible',
-      children: elementCloned.props.children ? settingUniqueStructureId(elementCloned.props.children) : null,
-    });
+    const newStructure = cloningWithUniqueId(initialState);
 
     structuresArray.push(newStructure);
     setStructure(structuresArray);
@@ -69,11 +92,30 @@ const CloneElement = ({ children, buttonText, buttonType, buttonIcon, removableF
   }, []);
 
   return (
-    <CloneElementContainer>
+    <CloneElementContainer {...buttonProps}>
       {structure}
       <Button onClick={handleAddStructure} label={buttonText} type={buttonType} icon={buttonIcon} />
     </CloneElementContainer>
   );
+};
+
+CloneElement.propTypes = {
+  children: PropTypes.node.isRequired,
+  buttonText: PropTypes.string,
+  buttonType: PropTypes.string,
+  buttonIcon: PropTypes.func,
+  removableFirst: PropTypes.bool,
+  buttonProps: PropTypes.shape({}),
+  onDeleteCallback: PropTypes.func,
+};
+
+CloneElement.defaultProps = {
+  buttonText: '',
+  buttonType: 'link-default-left',
+  buttonIcon: null,
+  removableFirst: true,
+  buttonProps: {},
+  onDeleteCallback: () => null,
 };
 
 export default CloneElement;
