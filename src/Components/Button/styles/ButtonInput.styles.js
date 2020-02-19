763644be566a8047df149040e9b93946479fd5b1
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2020-02-18 22:11:45 -0300

    Setting styles props in button to handle overflow content

--- src/Components/Button/styles/ButtonInput.styles.js
+++ src/Components/Button/styles/ButtonInput.styles.js
@@ -3,4 +3,6 @@ import styled from 'styled-components';
 export default styled.a`
   display: inline-block;
   pointer-events: none;
+  max-width: ${({ maxWidth }) => maxWidth};
+  overflow: ${({ overflow }) => overflow};
 `;
