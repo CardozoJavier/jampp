1e1ab9f5a83e2eecfe1537c80f716260aa7206c8
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2020-02-25 16:06:27 -0300

    Refactor in getQueryParams for return null if url is not valid

--- src/utils/getQueryParams.js
+++ src/utils/getQueryParams.js
@@ -2,14 +2,18 @@
  * Function to get query params from an URL
  */
 const getQueryParams = (url) => {
-  const a = document.createElement('a');
-  a.href = url;
-  const params = new URLSearchParams(a.search);
-  const paramsDecoded = decodeURIComponent(params);
-  const paramsSplitted = paramsDecoded.split('&').map(param => param.split('='));
-  const paramsMapped = new Map(paramsSplitted);
-  
-  return paramsMapped;
+  if (!url) return null;
+  try {
+    const newUrl = new URL(url);
+    const params = new URLSearchParams(newUrl.search);
+    const paramsDecoded = decodeURIComponent(params);
+    const paramsSplitted = paramsDecoded.split('&').map(param => param.split('='));
+    const paramsMapped = new Map(paramsSplitted);
+
+    return paramsMapped;
+  } catch (e) {
+    return null;
+  };
 }
 
 export default getQueryParams;
