2d3d05099d07e071a3eb5d535d8bc08cb4cc8e1e
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2020-03-06 19:56:29 -0300

    ToggleSwitch component changed to controlled component

--- stories/Toggle.stories.js
+++ stories/Toggle.stories.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { ToggleSwitch } from '../src/Components';
-
 export default {
   title: 'Toggle',
 };
@@ -10,12 +9,12 @@ export default {
  * @prop {String} color - (Optional) Used for setting the active color on switch. Can be 'green' (default), 'blue', 'red', 'yellow'.
  * @prop {Function} onChange - (Optional) Callback to trigger on onChange event. It receive one argument (true or false).
  * @prop {Boolean} disabled - (Optional) If true, disable actions triggering and styles in component.
- * @prop {Boolean} defaultValue - (Optional) Define true/false status. Default value is false.
+ * @prop {Boolean} status - (Required) Define true/false status. Default value is false.
  */
 export const Toggle = () => (
   <>
-    <ToggleSwitch onChange={(status) => console.log('Toggle status is ' + status)} />
-    <ToggleSwitch defaultValue={true} onChange={(status) => console.log('Toggle status is ' + status)} />
+    <ToggleSwitch status={false} onChange={(status) => console.log('Toggle status is ' + status)} />
+    <ToggleSwitch status={true} onChange={(status) => console.log('Toggle status is ' + status)} />
     <ToggleSwitch disabled={true} />
   </>
 );
