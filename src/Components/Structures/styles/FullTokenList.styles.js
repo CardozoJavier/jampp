cc9013f98b8b1c25548994ea3283b2f1ba59f9c1
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2020-03-28 17:32:54 -0300

    FullTokenList component created

---
+++ src/Components/Structures/styles/FullTokenList.styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+import { PaymentRowTitle } from '../../Table/styles';
+
+export const ModalContainer = styled.div`
+  position: absolute;
+  bottom: 250px;
+  right: -37.5px;
+`;
+
+export const TokenListContainer = styled.div`
+  max-height: 214px;
+  overflow: scroll;
+`;
+
+export const FullTokenListRowTitle = styled(PaymentRowTitle)`
+  padding: 0;
+  margin: 0 22px;
+`;
