8697687424a0fd832acb6f6264e490d4fa9ea494
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2020-05-31 20:53:23 -0300

    Add test for Step component

--- src/Components/Breadcrumb/Step.js
+++ src/Components/Breadcrumb/Step.js
@@ -1,26 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { ArrowIcon, CheckIcon } from '../UI/Icons';
-import { StepContainer, IconContainer, IconCircle, StepText } from './styles';
+import {
+  StepContainer, IconContainer, IconCircle, StepText,
+} from './styles';
 
 /**
  * Step component should be called with
  * @param {String} text - (Required) It's the name step.
- * @param {Boolean} isDone - (Optional) Define if the step is done for styling breadcrumb. 
- * @param {Boolean} isCurrent - (Optional) Define if is the current step. 
+ * @param {Boolean} isDone - (Optional) Define if the step is done for styling breadcrumb.
+ * @param {Boolean} isCurrent - (Optional) Define if is the current step.
  * @param {Boolean} isLast - (Optional) Define if is the last step and not display ArrowIcon.
  * @param {String} borderColor - (Optional) Define the border color of check icon regarding step is done/current/next.
  */
-const Step = ({ text, isDone = false, isCurrent = false, isLast = false, borderColor = '' }) => (
+const Step = ({
+  text, isDone = false, isCurrent = false, isLast = false, borderColor = '',
+}) => (
   <StepContainer>
     <IconContainer>
       <CheckIcon props={{ disabled: true }} />
       <IconCircle borderColor={borderColor} isDone={isDone} />
     </IconContainer>
     <StepText isCurrent={isCurrent}>{text}</StepText>
-    {!isLast &&
-      <ArrowIcon props={{ margin: '0 10px' }} />
-    }
+    {!isLast
+      && <ArrowIcon props={{ margin: '0 10px' }} />}
   </StepContainer>
 );
 
@@ -29,7 +32,11 @@ Step.propTypes = {
   isDone: PropTypes.bool.isRequired,
   isCurrent: PropTypes.bool.isRequired,
   isLast: PropTypes.bool.isRequired,
-  borderColor: PropTypes.string.isRequired,
+  borderColor: PropTypes.string,
+};
+
+Step.defaultProps = {
+  borderColor: '',
 };
 
 export default Step;
---
+++ src/Components/Breadcrumb/__tests__/Step.spec.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-undef */
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Step from '../Step';
+
+describe('<Step /> component', () => {
+  const text = 'Test';
+
+  test('should render text', () => {
+    render(<Step text={text} isLast={false} isCurrent isNext={false} isDone={false} />);
+    expect(screen.getByText(text)).not.toBeNull();
+  });
+
+  test('should match snapshot', () => {
+    const { container } = render(<Step text={text} isLast={false} isCurrent isNext={false} isDone={false} />);
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
