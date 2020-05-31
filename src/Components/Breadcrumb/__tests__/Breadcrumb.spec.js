799f55a75d7d8214ea762166f46ee2256c887d42
Javier Andres Cardozo <javier.cardozo@mercadolibre.com>
2020-05-31 01:07:06 -0300

    Add tests for Breadcrumb component

---
+++ src/Components/Breadcrumb/__tests__/Breadcrumb.spec.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-undef */
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Breadcrumb from '../Breadcrumb';
+
+describe('<Breadcrumb /> component', () => {
+  const children = [
+    <div key="key1" text="text 1" className="testing-length" />,
+    <div key="key2" text="text 2" className="testing-length" />,
+    <div key="key3" text="text 3" className="testing-length" />,
+    <div key="key4" text="text 4" className="testing-length" />,
+  ];
+  test('should render children', () => {
+    render(<Breadcrumb>{children}</Breadcrumb>);
+    expect(screen.getAllByText(/text/)).toHaveLength(children.length);
+  });
+});
